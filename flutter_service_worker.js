ad0cbf8b09887124187a1eef8d5b3d42b38b808a
csnoboa <caique2noboa@gmail.com>
2020-07-14 09:40:53 -0300

    Beta v1.3.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4cdf7fc0bea8990af488f6246f9e4150",
 "/": "4cdf7fc0bea8990af488f6246f9e4150",
-"main.dart.js": "a192d8171827b8863a14fe7255120b8b",
+"main.dart.js": "ee1655cbe8abc4051b025178499a5fd1",
 "manifest.json": "012ee4da57faecdb6f32443340f350e5"
 };
 
