6d8e3f42cc45ebcdd0ee2f2e238dd5494ef8b4a5
csnoboa <caique2noboa@gmail.com>
2020-07-30 10:14:06 -0300

    TV-26

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4cdf7fc0bea8990af488f6246f9e4150",
 "/": "4cdf7fc0bea8990af488f6246f9e4150",
-"main.dart.js": "768fe9cc66f8af55542f92327cbc6480",
+"main.dart.js": "016f5a5c48398f76a2df47fa317b83f6",
 "manifest.json": "012ee4da57faecdb6f32443340f350e5"
 };
 
