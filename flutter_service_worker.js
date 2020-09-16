7448ebe809541070003a6e9bd97deffd0a11f13c
csnoboa <caique2noboa@gmail.com>
2020-09-15 22:27:08 -0300

    1.2.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,13 +8,13 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
 "assets/NOTICES": "4bf73b9c652eb33930513f522f778c74",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/version.txt": "872ccd9c6dce18ce6ea4d5106540f089",
+"assets/version.txt": "62abc69fd3fad42d4dadca97c5d57105",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4cdf7fc0bea8990af488f6246f9e4150",
 "/": "4cdf7fc0bea8990af488f6246f9e4150",
-"main.dart.js": "29bc84c5d501db0f04ec88d916acd059",
+"main.dart.js": "94865c6acca087456af3b7995c3d25ec",
 "manifest.json": "012ee4da57faecdb6f32443340f350e5"
 };
 
