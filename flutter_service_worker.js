0eb161d4a31c94f59ad6f4d3433c4c31fe8a885f
csnoboa <caique2noboa@gmail.com>
2020-09-29 12:10:41 -0300

    1.2.5 Fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
 "assets/NOTICES": "4bf73b9c652eb33930513f522f778c74",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/version.txt": "08b3d4e36caf0fd8570aee1dc4acb64e",
+"assets/version.txt": "db3daece29a9d5f93d193ed25a4b973b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
